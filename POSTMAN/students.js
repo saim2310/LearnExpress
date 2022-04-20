e30f4997aeaa90eb78c0a16bee4e6c32149683b8
saim2310 <101701528+saim2310@users.noreply.github.com>
2022-04-21 00:10:52 +0500

    Add files via upload

---
+++ POSTMAN/students.js
@@ -0,0 +1,23 @@
+
+const students=[
+
+    {
+        "name":"saim",
+        "pass":"1234",
+    },
+    {
+        "name":"rafi",
+        "pass":"12345",
+    },
+    {
+        "name":"hassan",
+        "pass":"123456",
+    },
+    {
+        "name":"jahangir",
+        "pass":"1234567",
+    }
+]
+
+
+module.exports= students;
